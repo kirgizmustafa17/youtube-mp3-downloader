3f847e4599a04a211972a29392c3c75cad675166
kirgizmustafa17 <55243398+kirgizmustafa17@users.noreply.github.com>
2022-09-14 23:20:46 +0300

    Update watch.py

--- pages/watch.py
+++ pages/watch.py
@@ -1,5 +1,20 @@
 import streamlit as st
+from .. st_youtube_mp3 import ydl_opts
+
+ydl_opts = ydl_opts
 
 params = st.experimental_get_query_params()
 
-st.info(params)
+def downloader(_url = ""):
+    if _url != "":
+        url = _url
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.add_post_processor(MyCustomPP(), when='post_process')
+            ydl.download([url])
+    except yt_dlp.utils.DownloadError:
+        st.error("DownloadError")
+
+if len(params) > 0:
+    downloader(params['v'][0])

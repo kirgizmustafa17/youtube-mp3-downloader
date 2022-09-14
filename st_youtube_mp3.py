954f32fef2ceb2ef1a4096ff96b621a3c807088e
kirgizmustafa17 <55243398+kirgizmustafa17@users.noreply.github.com>
2022-09-14 22:36:05 +0300

    Update st_youtube_mp3.py

--- st_youtube_mp3.py
+++ st_youtube_mp3.py
@@ -5,6 +5,7 @@ import yt_dlp
 from _VALID_URL import _VALID_URL
 import time
 
+print(f"**********\n**********\**********\n{st.experimental_get_query_params()}\n**********\n**********\n**********")
 
 st.title("music.Youtube Downloader")
 

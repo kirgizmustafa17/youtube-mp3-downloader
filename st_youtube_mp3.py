01e5e8281e8a8ec2fb78686ff78152bf2a1116ff
kirgizmustafa17 <55243398+kirgizmustafa17@users.noreply.github.com>
2022-09-14 21:24:15 +0300

    Update st_youtube_mp3.py

--- st_youtube_mp3.py
+++ st_youtube_mp3.py
@@ -110,7 +110,7 @@ if url != old_url:  # or btn
 
     for f in os.listdir("."):
         if f.endswith(".mp3"):
-            print(f""-----------mtime:\t{os.stat(f).st_mtime}")
+            print(f"-----------mtime:\t{os.stat(f).st_mtime}")
             if os.stat(f).st_mtime < now - 3600:
                 os.remove(os.path.join(".", f))
                 print(f"REMOVED {f}")
